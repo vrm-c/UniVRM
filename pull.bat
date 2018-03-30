52e24efc5f8db1dff25f9bc16e8a761ecc22eb5a
G21 <G21@G21>
2018-03-30 19:04:55 +0900

    fix pull.bat

--- pull.bat
+++ pull.bat
@@ -1,7 +1,7 @@
 ::pushd Assets\VRM
 git pull
 git submodule init
-::git submodule update --remote
+git submodule update --remote
 git submodule foreach git checkout master
 git submodule foreach git pull
 ::popd
