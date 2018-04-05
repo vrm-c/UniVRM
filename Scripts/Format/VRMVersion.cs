1c70c1487a87c4eef4e960fbc7f62cb67f2e529f
ousttrue <ousttrue@gmail.com>
2018-04-05 16:14:56 +0900

    increment version

--- Scripts/Format/VRMVersion.cs
+++ Scripts/Format/VRMVersion.cs
@@ -4,11 +4,11 @@ namespace VRM
     public static class VRMVersion
     {
         public const int MAJOR = 0;
-        public const int MINOR = 12;
+        public const int MINOR = 13;
 
-        public const string VERSION = "0.12";
+        public const string VERSION = "0.13";
 
-        public const string DecrementMenuName = "VRM/Version(0.12) Decrement";
-        public const string IncrementMenuName = "VRM/Version(0.12) Increment";
+        public const string DecrementMenuName = "VRM/Version(0.13) Decrement";
+        public const string IncrementMenuName = "VRM/Version(0.13) Increment";
     }
 }
