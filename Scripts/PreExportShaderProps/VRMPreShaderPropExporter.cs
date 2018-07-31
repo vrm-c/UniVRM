bd345ccfcd2cc5ac668b18914b6391cb5cb8b753
ousttrue <ousttrue@gmail.com>
2018-07-31 22:11:34 +0900

    #if UNITY_EDITOR

--- Scripts/PreExportShaderProps/VRMPreShaderPropExporter.cs
+++ Scripts/PreExportShaderProps/VRMPreShaderPropExporter.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 using UniGLTF;
-using UnityEditor;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 
 namespace VRM
