6437e158a640a9ce015253fa488e7ee5e95c968c
ousttrue <ousttrue@gmail.com>
2018-08-17 22:50:05 +0900

    Incremented version

--- Scripts/Format/VRMVersion.cs
+++ Scripts/Format/VRMVersion.cs
@@ -4,8 +4,8 @@ namespace VRM
     public static partial class VRMVersion
     {
         public const int MAJOR = 0;
-        public const int MINOR = 41;
+        public const int MINOR = 42;
 
-        public const string VERSION = "0.41";
+        public const string VERSION = "0.42";
     }
 }
