d4a27eb4b25cbe4254cf24391c39bb1c6c482a3e
Nakaji Kohki <boomerang.cube@gmail.com>
2018-09-30 19:45:58 +0900

    Assign unused arguments

--- Scripts/BlendShape/Editor/BlendShapeClipEditor.cs
+++ Scripts/BlendShape/Editor/BlendShapeClipEditor.cs
@@ -279,7 +279,7 @@ namespace VRM
         static bool FloatSlider(Rect rect, SerializedProperty prop, float maxValue)
         {
             var oldValue = prop.floatValue;
-            var newValue = EditorGUI.Slider(rect, prop.floatValue, 0, 100f);
+            var newValue = EditorGUI.Slider(rect, prop.floatValue, 0, maxValue);
             if (newValue != oldValue)
             {
                 prop.floatValue = newValue;
