b05a51c02cf572ab1cf4566c372a01d979b4ce2a
ousttrue <ousttrue@gmail.com>
2018-12-19 16:49:38 +0900

    Restore BlendShapeClip.Apply

--- Scripts/BlendShape/BlendShapeClip.cs
+++ Scripts/BlendShape/BlendShapeClip.cs
@@ -58,6 +58,50 @@ namespace VRM
                 return m_prefab;
             }
         }
+
+        /// <summary>
+        /// Apply BlendShape for Preview
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="value"></param>
+        public void Apply(Transform root, float value)
+        {
+            if (Values != null)
+            {
+                foreach (var x in Values)
+                {
+                    var target = root.Find(x.RelativePath);
+                    if (target != null)
+                    {
+                        var sr = target.GetComponent<SkinnedMeshRenderer>();
+                        if (sr != null)
+                        {
+                            sr.SetBlendShapeWeight(x.Index, x.Weight * value);
+                        }
+                    }
+                }
+            }
+
+            /*
+            if (MaterialValues != null)
+            {
+                foreach (var x in MaterialValues)
+                {
+                    var target = root.Find(x.RelativePath);
+                    if (target != null)
+                    {
+                        var sr = target.GetComponent<SkinnedMeshRenderer>();
+                        if (sr != null)
+                        {
+                            var m = sr.sharedMaterials[x.Index];
+                            var color = x.BaseValue + (x.TargetValue - x.BaseValue) * value;
+                            m.SetColor(x.ValueName, color);
+                        }
+                    }
+                }
+            }
+            */
+        }
 #endif
 
         /// <summary>
