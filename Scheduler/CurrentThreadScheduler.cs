f20f27259c28dbff6697958b8727d39d24f74ce1
ousttrue <ousttrue@gmail.com>
2018-12-28 21:15:54 +0900

    Squashed 'DepthFirstScheduler/' content from commit d0c62ba
    git-subtree-dir: DepthFirstScheduler
    git-subtree-split: d0c62ba0fbef280af0ef5ec41963bef847185efb

---
+++ Scheduler/CurrentThreadScheduler.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DepthFirstScheduler
+{
+    public static partial class Scheduler
+    {
+        private static IScheduler currentThread;
+
+        public static IScheduler CurrentThread
+        {
+            get { return currentThread ?? (currentThread = new CurrentThreadScheduler()); }
+        }
+
+        public class CurrentThreadScheduler : IScheduler
+        {
+            [ThreadStatic]
+            private static Queue<TaskChain> queue;
+
+            private static Queue<TaskChain> GetQueue()
+            {
+                return queue;
+            }
+
+            private static void SetQueue(Queue<TaskChain> newQueue)
+            {
+                queue = newQueue;
+            }
+
+            public void Enqueue(TaskChain item)
+            {
+                var q = GetQueue();
+
+                if (q == null)
+                {
+                    q = new Queue<TaskChain>(5);
+                    q.Enqueue(item);
+                    SetQueue(q);
+
+                    try
+                    {
+                        Trampoline.Run(q);
+                    }
+                    finally
+                    {
+                        SetQueue(null);
+                    }
+                }
+                else
+                {
+                    q.Enqueue(item);
+                }
+            }
+
+            #region IDisposable Support
+
+            private bool disposedValue = false;
+
+            protected virtual void Dispose(bool disposing)
+            {
+                if (!disposedValue)
+                {
+                    if (disposing)
+                    {
+                        var queue = GetQueue();
+                        if (queue != null) queue.Clear();
+                        SetQueue(null);
+                    }
+                    disposedValue = true;
+                }
+            }
+
+            public void Dispose()
+            {
+                Dispose(true);
+            }
+
+            #endregion
+        }
+
+        static class Trampoline
+        {
+            public static void Run(Queue<TaskChain> queue)
+            {
+                while (queue.Count > 0)
+                {
+                    var chain = queue.Dequeue();
+
+                    while (true)
+                    {
+                        var status = chain.Next();
+                        if (status != ExecutionStatus.Continue)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
