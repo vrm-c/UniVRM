60041f70c3a1cc159f48be13e4b0dc2d62f8eb54
yutopp <yutopp@gmail.com>
2019-01-08 16:51:39 +0900

    Add a test for JsonSchema.Serialize

---
+++ Assets/VRM/UniJSON/Editor/Tests/Json/SerializeWithSchemaTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+
+namespace UniJSON
+{
+    public class SerializeWithSchemaTests
+    {
+        [JsonSchema(Title="CheckConstraintsTest")]
+        public class CheckConstraintsTest
+        {
+            [JsonSchema(Minimum = 0)]
+            public int X;
+
+            [JsonSchema(Minimum = 10)] // Not required, thus ignored when the value violates the constraints
+            public int Y;
+        }
+
+        [Test]
+        public void TestCheckConstraints()
+        {
+            var obj = new CheckConstraintsTest()
+            {
+                X = 0,
+                Y = 0, // Will be excluded because 0 doesn't satisfy a requirement of "Minimum = 10"
+            };
+
+            var s = JsonSchema.FromType<CheckConstraintsTest>();
+            {
+                var c = new JsonSchemaValidationContext(obj);
+                Assert.Null(s.Validator.Validate(c, s));
+            }
+            var actual = s.Serialize(obj);
+
+            var expected = @"{""X"":0}";
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
