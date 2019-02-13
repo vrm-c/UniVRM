befe81578d94b3003c51e91e80e864a343f5ab87
ousttrue <ousttrue@gmail.com>
2019-02-13 17:05:41 +0900

    Add dummy generics call for AOT

---
+++ Assets/VRM/UniVRM/Editor/Format/VRMAOTMenu.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using UniGLTF;
+using UnityEditor;
+using UnityEngine;
+
+
+namespace VRM
+{
+    public static class VRMAOTMenu
+    {
+        /// <summary>
+        /// AOT向けにダミーのGenerics呼び出しを作成する
+        /// </summary>
+#if VRM_DEVELOP
+        [MenuItem(VRMVersion.MENU + "/GenerateAOTCall")]
+#endif
+        static void GenerateAOTCall()
+        {
+            var path = UnityPath.FromUnityPath("Assets/VRM/UniVRM/Scripts/AOTCall.g.cs");
+            var encoding = new UTF8Encoding(false);
+            using (var s = new MemoryStream())
+            {
+                using (var w = new StreamWriter(s, encoding))
+                {
+                    w.WriteLine(@"
+using System;
+using UniJSON;
+using UniGLTF;
+using System.Collections.Generic;
+
+
+namespace VRM {
+    public static partial class VRMAOTCall {
+        static void glTF()
+        {       
+            var f = new JsonFormatter();
+");
+
+                    foreach (var t in TraverseType(typeof(glTF), new List<Type>
+                {
+                    typeof(object),
+                    typeof(string),
+                    typeof(bool),
+
+                    typeof(byte),
+                    typeof(ushort),
+                    typeof(uint),
+                    typeof(ulong),
+
+                    typeof(sbyte),
+                    typeof(short),
+                    typeof(int),
+                    typeof(long),
+
+                    typeof(float),
+                    typeof(double),
+
+                    typeof(Vector3),
+                }))
+                    {
+                        var typeName = t.Name;
+                        var listType = GetGenericListType(t);
+                        if (listType != null)
+                        {
+                            typeName = "List<$0>".Replace("$0", listType.Name);
+                        }
+                        var dictType = GetDictionaryValueType(t);
+                        if (dictType != null)
+                        {
+                            typeName = "Dictionary<string, $0>".Replace("$0", dictType.Name);
+                        }
+
+                        w.WriteLine("f.Serialize(default($0));".Replace("$0", typeName));
+                        w.WriteLine(@"{
+    var value = default($0);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}".Replace("$0", typeName));
+                        w.WriteLine();
+                    }
+
+                    w.WriteLine(@"
+        }
+    }
+}
+");
+                }
+
+                var text = encoding.GetString(s.ToArray());
+                File.WriteAllText(path.FullPath, text.Replace("\r\n", "\n"), encoding);
+            }
+
+            path.ImportAsset();
+        }
+
+        static Type GetGenericListType(Type t)
+        {
+            if(t.IsGenericType 
+                && t.GetGenericTypeDefinition() == typeof(List<>))
+            {
+                return t.GetGenericArguments()[0];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        static Type GetDictionaryValueType(Type t)
+        {
+            if (t.IsGenericType
+                && t.GetGenericTypeDefinition() == typeof(Dictionary<,>)
+                && t.GetGenericArguments()[0] == typeof(string))
+            {
+                return t.GetGenericArguments()[1];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        static IEnumerable<Type> TraverseType(Type t, List<Type> excludes)
+        {
+            if (excludes.Contains(t))
+            {
+                yield break;
+            }
+
+            Debug.LogFormat("{0}", t);
+            excludes.Add(t);
+            yield return t;
+
+            var listType = GetGenericListType(t);
+            if (listType!=null)
+            {
+                foreach (var x in TraverseType(listType, excludes))
+                {
+                    yield return x;
+                }
+            }
+            else
+            {
+                foreach (var fi in t.GetFields(BindingFlags.Public | BindingFlags.Instance))
+                {
+                    foreach (var x in TraverseType(fi.FieldType, excludes))
+                    {
+                        yield return x;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/VRM/UniVRM/Scripts/AOTCall.g.cs
@@ -0,0 +1,624 @@
+
+using System;
+using UniJSON;
+using UniGLTF;
+using System.Collections.Generic;
+
+
+namespace VRM {
+    public static partial class VRMAOTCall {
+        static void glTF()
+        {       
+            var f = new JsonFormatter();
+
+f.Serialize(default(glTF));
+{
+    var value = default(glTF);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTFAssets));
+{
+    var value = default(glTFAssets);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(List<glTFBuffer>));
+{
+    var value = default(List<glTFBuffer>);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTFBuffer));
+{
+    var value = default(glTFBuffer);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(List<glTFBufferView>));
+{
+    var value = default(List<glTFBufferView>);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTFBufferView));
+{
+    var value = default(glTFBufferView);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glBufferTarget));
+{
+    var value = default(glBufferTarget);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(List<glTFAccessor>));
+{
+    var value = default(List<glTFAccessor>);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTFAccessor));
+{
+    var value = default(glTFAccessor);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glComponentType));
+{
+    var value = default(glComponentType);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(Single[]));
+{
+    var value = default(Single[]);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTFSparse));
+{
+    var value = default(glTFSparse);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTFSparseIndices));
+{
+    var value = default(glTFSparseIndices);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTFSparseValues));
+{
+    var value = default(glTFSparseValues);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(List<glTFTexture>));
+{
+    var value = default(List<glTFTexture>);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTFTexture));
+{
+    var value = default(glTFTexture);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(List<glTFTextureSampler>));
+{
+    var value = default(List<glTFTextureSampler>);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTFTextureSampler));
+{
+    var value = default(glTFTextureSampler);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glFilter));
+{
+    var value = default(glFilter);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glWrap));
+{
+    var value = default(glWrap);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(List<glTFImage>));
+{
+    var value = default(List<glTFImage>);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTFImage));
+{
+    var value = default(glTFImage);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(List<glTFMaterial>));
+{
+    var value = default(List<glTFMaterial>);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTFMaterial));
+{
+    var value = default(glTFMaterial);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTFPbrMetallicRoughness));
+{
+    var value = default(glTFPbrMetallicRoughness);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTFMaterialBaseColorTextureInfo));
+{
+    var value = default(glTFMaterialBaseColorTextureInfo);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTFMaterialMetallicRoughnessTextureInfo));
+{
+    var value = default(glTFMaterialMetallicRoughnessTextureInfo);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTFMaterialNormalTextureInfo));
+{
+    var value = default(glTFMaterialNormalTextureInfo);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTFMaterialOcclusionTextureInfo));
+{
+    var value = default(glTFMaterialOcclusionTextureInfo);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTFMaterialEmissiveTextureInfo));
+{
+    var value = default(glTFMaterialEmissiveTextureInfo);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTFMaterial_extensions));
+{
+    var value = default(glTFMaterial_extensions);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTF_KHR_materials_unlit));
+{
+    var value = default(glTF_KHR_materials_unlit);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(List<glTFMesh>));
+{
+    var value = default(List<glTFMesh>);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTFMesh));
+{
+    var value = default(glTFMesh);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(List<glTFPrimitives>));
+{
+    var value = default(List<glTFPrimitives>);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTFPrimitives));
+{
+    var value = default(glTFPrimitives);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTFAttributes));
+{
+    var value = default(glTFAttributes);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(List<gltfMorphTarget>));
+{
+    var value = default(List<gltfMorphTarget>);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(gltfMorphTarget));
+{
+    var value = default(gltfMorphTarget);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTFPrimitives_extras));
+{
+    var value = default(glTFPrimitives_extras);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(List<String>));
+{
+    var value = default(List<String>);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTFPrimitives_extensions));
+{
+    var value = default(glTFPrimitives_extensions);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(List<glTFNode>));
+{
+    var value = default(List<glTFNode>);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTFNode));
+{
+    var value = default(glTFNode);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(Int32[]));
+{
+    var value = default(Int32[]);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTFNode_extensions));
+{
+    var value = default(glTFNode_extensions);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTFNode_extra));
+{
+    var value = default(glTFNode_extra);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(List<glTFSkin>));
+{
+    var value = default(List<glTFSkin>);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTFSkin));
+{
+    var value = default(glTFSkin);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(List<gltfScene>));
+{
+    var value = default(List<gltfScene>);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(gltfScene));
+{
+    var value = default(gltfScene);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(List<glTFAnimation>));
+{
+    var value = default(List<glTFAnimation>);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTFAnimation));
+{
+    var value = default(glTFAnimation);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(List<glTFAnimationChannel>));
+{
+    var value = default(List<glTFAnimationChannel>);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTFAnimationChannel));
+{
+    var value = default(glTFAnimationChannel);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTFAnimationTarget));
+{
+    var value = default(glTFAnimationTarget);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(List<glTFAnimationSampler>));
+{
+    var value = default(List<glTFAnimationSampler>);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTFAnimationSampler));
+{
+    var value = default(glTFAnimationSampler);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(List<glTFCamera>));
+{
+    var value = default(List<glTFCamera>);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTFCamera));
+{
+    var value = default(glTFCamera);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTFOrthographic));
+{
+    var value = default(glTFOrthographic);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTFOrthographic_extensions));
+{
+    var value = default(glTFOrthographic_extensions);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTFOrthographic_extras));
+{
+    var value = default(glTFOrthographic_extras);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTFPerspective));
+{
+    var value = default(glTFPerspective);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTFPerspective_extensions));
+{
+    var value = default(glTFPerspective_extensions);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTFPerspective_extras));
+{
+    var value = default(glTFPerspective_extras);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(ProjectionType));
+{
+    var value = default(ProjectionType);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTFCamera_extensions));
+{
+    var value = default(glTFCamera_extensions);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTFCamera_extras));
+{
+    var value = default(glTFCamera_extras);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTF_extensions));
+{
+    var value = default(glTF_extensions);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTF_VRM_extensions));
+{
+    var value = default(glTF_VRM_extensions);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTF_VRM_Meta));
+{
+    var value = default(glTF_VRM_Meta);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTF_VRM_Humanoid));
+{
+    var value = default(glTF_VRM_Humanoid);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(List<glTF_VRM_HumanoidBone>));
+{
+    var value = default(List<glTF_VRM_HumanoidBone>);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTF_VRM_HumanoidBone));
+{
+    var value = default(glTF_VRM_HumanoidBone);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTF_VRM_Firstperson));
+{
+    var value = default(glTF_VRM_Firstperson);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(List<glTF_VRM_MeshAnnotation>));
+{
+    var value = default(List<glTF_VRM_MeshAnnotation>);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTF_VRM_MeshAnnotation));
+{
+    var value = default(glTF_VRM_MeshAnnotation);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTF_VRM_DegreeMap));
+{
+    var value = default(glTF_VRM_DegreeMap);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTF_VRM_BlendShapeMaster));
+{
+    var value = default(glTF_VRM_BlendShapeMaster);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(List<glTF_VRM_BlendShapeGroup>));
+{
+    var value = default(List<glTF_VRM_BlendShapeGroup>);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTF_VRM_BlendShapeGroup));
+{
+    var value = default(glTF_VRM_BlendShapeGroup);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(List<glTF_VRM_BlendShapeBind>));
+{
+    var value = default(List<glTF_VRM_BlendShapeBind>);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTF_VRM_BlendShapeBind));
+{
+    var value = default(glTF_VRM_BlendShapeBind);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(List<glTF_VRM_MaterialValueBind>));
+{
+    var value = default(List<glTF_VRM_MaterialValueBind>);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTF_VRM_MaterialValueBind));
+{
+    var value = default(glTF_VRM_MaterialValueBind);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTF_VRM_SecondaryAnimation));
+{
+    var value = default(glTF_VRM_SecondaryAnimation);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(List<glTF_VRM_SecondaryAnimationGroup>));
+{
+    var value = default(List<glTF_VRM_SecondaryAnimationGroup>);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTF_VRM_SecondaryAnimationGroup));
+{
+    var value = default(glTF_VRM_SecondaryAnimationGroup);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(List<glTF_VRM_SecondaryAnimationColliderGroup>));
+{
+    var value = default(List<glTF_VRM_SecondaryAnimationColliderGroup>);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTF_VRM_SecondaryAnimationColliderGroup));
+{
+    var value = default(glTF_VRM_SecondaryAnimationColliderGroup);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(List<glTF_VRM_SecondaryAnimationCollider>));
+{
+    var value = default(List<glTF_VRM_SecondaryAnimationCollider>);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTF_VRM_SecondaryAnimationCollider));
+{
+    var value = default(glTF_VRM_SecondaryAnimationCollider);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(List<glTF_VRM_Material>));
+{
+    var value = default(List<glTF_VRM_Material>);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(glTF_VRM_Material));
+{
+    var value = default(glTF_VRM_Material);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(Dictionary<string, Single>));
+{
+    var value = default(Dictionary<string, Single>);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(Dictionary<string, Single[]>));
+{
+    var value = default(Dictionary<string, Single[]>);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(Dictionary<string, Int32>));
+{
+    var value = default(Dictionary<string, Int32>);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(Dictionary<string, Boolean>));
+{
+    var value = default(Dictionary<string, Boolean>);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(Dictionary<string, String>));
+{
+    var value = default(Dictionary<string, String>);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+f.Serialize(default(gltf_extras));
+{
+    var value = default(gltf_extras);
+    default(ListTreeNode<JsonValue>).Deserialize(ref value);
+}
+
+
+        }
+    }
+}
+
