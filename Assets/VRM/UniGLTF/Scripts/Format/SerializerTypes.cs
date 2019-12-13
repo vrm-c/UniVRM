583f5c273a1933fd3df9a2bc924cd18d2759c2b3
hirose <j-hirose@virtualcast.jp>
2019-12-13 21:51:33 +0900

    add SerializerTypes.cs

---
+++ Assets/VRM/UniGLTF/Scripts/Format/SerializerTypes.cs
@@ -0,0 +1,9 @@
+﻿namespace UniGLTF
+{
+    public enum SerializerTypes
+    {
+        JsonSerializable, // manual
+        UniJSON, // reflection
+        Generated, // generated, experimental for mobile
+    }
+}
