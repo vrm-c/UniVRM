3e0c352edc37ff866afeb668b79e2b0cdfcf9a44
Kohei-Yanagida <k-yanagida@virtualcast.jp>
2019-12-17 21:36:39 +0900

    fix prefab revert error

--- Assets/VRM/UniVRM/Scripts/Format/VRMExportSettings.cs
+++ Assets/VRM/UniVRM/Scripts/Format/VRMExportSettings.cs
@@ -394,12 +394,15 @@ namespace VRM
                 File.WriteAllBytes(path, bytes);
                 Debug.LogFormat("Export elapsed {0}", sw.Elapsed);
             }
-
+            if (IsPrefab(Source))
+            {
 #if UNITY_2018_3_OR_NEWER
-            PrefabUtility.RevertPrefabInstance(Source, InteractionMode.AutomatedAction);
+                PrefabUtility.RevertPrefabInstance(Source, InteractionMode.AutomatedAction);
 #else
-            PrefabUtility.RevertPrefabInstance(target);
+                PrefabUtility.RevertPrefabInstance(target);
 #endif
+            }
+
             if (path.StartsWithUnityAssetPath())
             {
                 AssetDatabase.ImportAsset(path.ToUnityRelativePath());
