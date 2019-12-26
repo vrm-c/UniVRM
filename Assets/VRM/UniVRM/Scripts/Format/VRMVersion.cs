225f55f0bb5e3a2f473fbe881b97be0d767ed425
hirose <j-hirose@virtualcast.jp>
2019-12-26 20:03:18 +0900

    update VERSION

--- Assets/VRM/UniVRM/Scripts/Format/VRMVersion.cs
+++ Assets/VRM/UniVRM/Scripts/Format/VRMVersion.cs
@@ -4,10 +4,10 @@ namespace VRM
     public static partial class VRMVersion
     {
         public const int MAJOR = 0;
-        public const int MINOR = 54;
+        public const int MINOR = 55;
         public const int PATCH = 0;
         public const string PRE_ID = "";
 
-        public const string VERSION = "0.54.0";
+        public const string VERSION = "0.55.0";
     }
 }
