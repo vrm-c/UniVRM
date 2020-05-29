3dcec1376f501d0e9607bffb38705aca7d95a5a5
hiroj <j-hirose@virtualcast.jp>
2020-05-29 15:23:32 +0900

    add CacheEnum

---
+++ Assets/VRM/UniGLTF/Editor/Tests/CacheEnumTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+
+namespace UniGLTF
+{
+    public class CacheEnumTest
+    {
+        [Test]
+        public void CacheEnumTestSimplePasses()
+        {
+            Assert.AreEqual(default(HumanBodyBones), CacheEnum.TryParseOrDefault<HumanBodyBones>("xxx"));
+
+#if UNITY_5_6_OR_NEWER
+            Assert.AreEqual(HumanBodyBones.UpperChest, CacheEnum.TryParseOrDefault<HumanBodyBones>("upperchest", true));
+#else
+            Assert.AreEqual(default(HumanBodyBones), CacheEnum.TryParseOrDefault<HumanBodyBones>("upperchest"));
+#endif
+        }
+    }
+}
