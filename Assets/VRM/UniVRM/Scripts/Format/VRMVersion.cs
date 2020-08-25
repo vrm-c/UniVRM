d005a7f5fd4156eb03b33cdea0e21936419a632b
ousttrue <ousttrue@gmail.com>
2020-08-25 21:04:27 +0900

    Bump version to 0.59.0

--- Assets/VRM/UniVRM/Scripts/Format/VRMVersion.cs
+++ Assets/VRM/UniVRM/Scripts/Format/VRMVersion.cs
@@ -4,8 +4,8 @@ namespace VRM
     public static partial class VRMVersion
     {
         public const int MAJOR = 0;
-        public const int MINOR = 58;
-        public const int PATCH = 1;
-        public const string VERSION = "0.58.1";
+        public const int MINOR = 59;
+        public const int PATCH = 0;
+        public const string VERSION = "0.59.0";
     }
 }
