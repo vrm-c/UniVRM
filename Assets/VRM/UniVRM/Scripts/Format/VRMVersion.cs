9a8153b3bcb85d5f05b4fbc6a527b6eab7c5c7ba
ousttrue <ousttrue@gmail.com>
2020-10-05 20:13:53 +0900

    Bump version to 0.61.0

--- Assets/VRM/UniVRM/Scripts/Format/VRMVersion.cs
+++ Assets/VRM/UniVRM/Scripts/Format/VRMVersion.cs
@@ -4,8 +4,8 @@ namespace VRM
     public static partial class VRMVersion
     {
         public const int MAJOR = 0;
-        public const int MINOR = 60;
+        public const int MINOR = 61;
         public const int PATCH = 0;
-        public const string VERSION = "0.60.0";
+        public const string VERSION = "0.61.0";
     }
 }
