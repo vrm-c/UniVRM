ba81098b53df5760dce35eca67efd70f745258b6
ousttrue <ousttrue@gmail.com>
2021-01-13 21:53:13 +0900

    update version 0.64.0

--- Assets/VRM/Runtime/Format/VRMVersion.cs
+++ Assets/VRM/Runtime/Format/VRMVersion.cs
@@ -4,8 +4,8 @@ namespace VRM
     public static partial class VRMVersion
     {
         public const int MAJOR = 0;
-        public const int MINOR = 63;
-        public const int PATCH = 2;
-        public const string VERSION = "0.63.2";
+        public const int MINOR = 64;
+        public const int PATCH = 0;
+        public const string VERSION = "0.64.0";
     }
 }
