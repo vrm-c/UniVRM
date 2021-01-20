24ff0d3fd1af65334cb599d696f324c01e0750bd
ousttrue <ousttrue@gmail.com>
2021-01-20 21:47:56 +0900

    MigrationMenu

---
+++ Assets/VRM10/Editor/MigrationMenu.cs
@@ -0,0 +1,46 @@
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace UniVRM10
+{
+    public static class MigrationMenu
+    {
+        static string s_lastPath = Application.dataPath;
+
+        const string CONTEXT_MENU = "Assets/Migration: Vrm1";
+
+        [MenuItem(CONTEXT_MENU, true)]
+        static bool Enable()
+        {
+            var path = UniGLTF.UnityPath.FromAsset(Selection.activeObject);
+            var isVrm = path.Extension.ToLower() == ".vrm";
+            return isVrm;
+        }
+
+        [MenuItem(CONTEXT_MENU, false)]
+        static void Exec()
+        {
+            var path = UniGLTF.UnityPath.FromAsset(Selection.activeObject);
+            var isVrm = path.Extension.ToLower() == ".vrm";
+
+            var vrm1Bytes = Migration.Migrate(File.ReadAllBytes(path.FullPath));
+
+            var dst = EditorUtility.SaveFilePanel(
+                "Save vrm1 file",
+                s_lastPath,
+                $"{path.FileName}_vrm1",
+                "vrm");
+            if (string.IsNullOrEmpty(dst))
+            {
+                return;
+            }
+
+            s_lastPath = Path.GetDirectoryName(dst);
+            File.WriteAllBytes(dst, vrm1Bytes);
+
+            // immediately import for GUI update
+            UniGLTF.UnityPath.FromFullpath(dst).ImportAsset();
+        }
+    }
+}
