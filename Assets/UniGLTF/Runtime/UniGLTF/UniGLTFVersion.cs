d3f56594da47ab46e66a604f6b17bc4f305a313c
ousttrue <ousttrue@gmail.com>
2021-02-01 15:22:41 +0900

    UniVRM-0.65.3 (UniGLTF-2.1.3)
    #711
    
    UniGLTFのバージョン番号は、 UniVRMのバージョン番号から機械的に決めることにします。
    更新の有無でそれぞれバージョンアップの有無を管理すると煩雑になるためです。
    
    major: 2
    minor: UniVRM.minor - 64
    patch: UniVRM.patch
    
    UniGLTF のバージョンを UniVRMのバージョンに揃えたいのですが、
    先に `1.27` というバージョンを持っていて巻き戻るので、とりあえず `2.0` に上げていてやむを得ない状態に。

--- Assets/UniGLTF/Runtime/UniGLTF/UniGLTFVersion.cs
+++ Assets/UniGLTF/Runtime/UniGLTF/UniGLTFVersion.cs
@@ -4,8 +4,8 @@ namespace UniGLTF
     public static partial class UniGLTFVersion
     {
         public const int MAJOR = 2;
-        public const int MINOR = 0;
-        public const int PATCH = 0;
-        public const string VERSION = "2.0.0";
+        public const int MINOR = 1;
+        public const int PATCH = 3;
+        public const string VERSION = "2.1.3";
     }
 }
