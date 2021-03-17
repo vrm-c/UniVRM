6bb0eebf81903a07c094026705a4298914113844
ousttrue <ousttrue@gmail.com>
2021-03-17 12:53:07 +0900

    DisposeOnGameObjectDestroyed後の BlendShapeClipのロードテストを追加

--- Assets/VRM.Samples/Editor/Tests/VRMImportExportTests.cs
+++ Assets/VRM.Samples/Editor/Tests/VRMImportExportTests.cs
@@ -51,63 +51,93 @@ namespace VRM.Samples
                 context.ShowMeshes();
                 context.EnableUpdateWhenOffscreen();
 
-                var importedJson = JsonParser.Parse(context.Json);
-                importedJson.SetValue("/extensions/VRM/exporterVersion", VRMVersion.VRM_VERSION, (f, x) => f.Value(x));
-                importedJson.SetValue("/asset/generator", UniGLTF.UniGLTFVersion.UNIGLTF_VERSION, (f, x) => f.Value(x));
-                importedJson.SetValue("/scene", 0, (f, x) => f.Value(x));
-                importedJson.SetValue("/materials/*/doubleSided", false, (f, x) => f.Value(x));
-                //importJson.SetValue("/materials/*/pbrMetallicRoughness/roughnessFactor", 0);
-                //importJson.SetValue("/materials/*/pbrMetallicRoughness/baseColorFactor", new float[] { 1, 1, 1, 1 });
-                importedJson.SetValue("/accessors/*/normalized", false, (f, x) => f.Value(x));
-                importedJson.RemoveValue(Utf8String.From("/nodes/*/extras"));
-                /*
-                importJson.SetValue("/bufferViews/12/byteStride", 4);
-                importJson.SetValue("/bufferViews/13/byteStride", 4);
-                importJson.SetValue("/bufferViews/14/byteStride", 4);
-                importJson.SetValue("/bufferViews/15/byteStride", 4);
-                importJson.SetValue("/bufferViews/22/byteStride", 4);
-                importJson.SetValue("/bufferViews/29/byteStride", 4);
-                importJson.SetValue("/bufferViews/45/byteStride", 4);
-                importJson.SetValue("/bufferViews/46/byteStride", 4);
-                importJson.SetValue("/bufferViews/47/byteStride", 4);
-                importJson.SetValue("/bufferViews/201/byteStride", 4);
-                importJson.SetValue("/bufferViews/202/byteStride", 4);
-                importJson.SetValue("/bufferViews/203/byteStride", 4);
-                importJson.SetValue("/bufferViews/204/byteStride", 4);
-                importJson.SetValue("/bufferViews/211/byteStride", 4);
-                importJson.SetValue("/bufferViews/212/byteStride", 4);
-                importJson.SetValue("/bufferViews/213/byteStride", 4);
-                importJson.SetValue("/bufferViews/214/byteStride", 4);
-                importJson.SetValue("/bufferViews/215/byteStride", 4);
-                importJson.SetValue("/bufferViews/243/byteStride", 4);
-                importJson.SetValue("/bufferViews/247/byteStride", 64);
-                importJson.SetValue("/bufferViews/248/byteStride", 64);
-                importJson.SetValue("/bufferViews/249/byteStride", 64);
-                importJson.SetValue("/bufferViews/250/byteStride", 64);
-                importJson.SetValue("/bufferViews/251/byteStride", 64);
-                importJson.SetValue("/bufferViews/252/byteStride", 64);
-                importJson.SetValue("/bufferViews/253/byteStride", 64);
-                */
-                importedJson.RemoveValue(Utf8String.From("/bufferViews/*/byteStride"));
-
-                var vrm = VRMExporter.Export(UniGLTF.MeshExportSettings.Default, context.Root);
-
-                // TODO: Check contents in JSON
-                /*var exportJson = */
-                JsonParser.Parse(vrm.ToJson());
-
-                // TODO: Check contents in JSON
-                /*var newExportedJson = */
-                // JsonParser.Parse(JsonSchema.FromType<glTF>().Serialize(vrm));
-
-                /*
-                foreach (var kv in importJson.Diff(exportJson))
+                var destroyer = context.DisposeOnGameObjectDestroyed();
+                try
                 {
-                    Debug.Log(kv);
-                }
+                    // meta
+                    {
+                        var meta = destroyer.GetComponent<VRMMeta>();
+                    }
 
-                Assert.AreEqual(importJson, exportJson);
-                */
+                    // humanoid
+                    {
+                        var animator = destroyer.GetComponent<Animator>();
+                    }
+
+
+                    // blendshape
+                    {
+                        var blendshapeProxy = destroyer.GetComponent<VRMBlendShapeProxy>();
+                        for (int i = 0; i < context.VRM.blendShapeMaster.blendShapeGroups.Count; ++i)
+                        {
+                            var gltfBlendShapeClip = context.VRM.blendShapeMaster.blendShapeGroups[i];
+                            var unityBlendShapeClip = blendshapeProxy.BlendShapeAvatar.Clips[i];
+                            Assert.AreEqual(Enum.Parse(typeof(BlendShapePreset), gltfBlendShapeClip.presetName, true), unityBlendShapeClip.Preset);
+                        }
+                    }
+
+                    var importedJson = JsonParser.Parse(context.Json);
+                    importedJson.SetValue("/extensions/VRM/exporterVersion", VRMVersion.VRM_VERSION, (f, x) => f.Value(x));
+                    importedJson.SetValue("/asset/generator", UniGLTF.UniGLTFVersion.UNIGLTF_VERSION, (f, x) => f.Value(x));
+                    importedJson.SetValue("/scene", 0, (f, x) => f.Value(x));
+                    importedJson.SetValue("/materials/*/doubleSided", false, (f, x) => f.Value(x));
+                    //importJson.SetValue("/materials/*/pbrMetallicRoughness/roughnessFactor", 0);
+                    //importJson.SetValue("/materials/*/pbrMetallicRoughness/baseColorFactor", new float[] { 1, 1, 1, 1 });
+                    importedJson.SetValue("/accessors/*/normalized", false, (f, x) => f.Value(x));
+                    importedJson.RemoveValue(Utf8String.From("/nodes/*/extras"));
+                    /*
+                    importJson.SetValue("/bufferViews/12/byteStride", 4);
+                    importJson.SetValue("/bufferViews/13/byteStride", 4);
+                    importJson.SetValue("/bufferViews/14/byteStride", 4);
+                    importJson.SetValue("/bufferViews/15/byteStride", 4);
+                    importJson.SetValue("/bufferViews/22/byteStride", 4);
+                    importJson.SetValue("/bufferViews/29/byteStride", 4);
+                    importJson.SetValue("/bufferViews/45/byteStride", 4);
+                    importJson.SetValue("/bufferViews/46/byteStride", 4);
+                    importJson.SetValue("/bufferViews/47/byteStride", 4);
+                    importJson.SetValue("/bufferViews/201/byteStride", 4);
+                    importJson.SetValue("/bufferViews/202/byteStride", 4);
+                    importJson.SetValue("/bufferViews/203/byteStride", 4);
+                    importJson.SetValue("/bufferViews/204/byteStride", 4);
+                    importJson.SetValue("/bufferViews/211/byteStride", 4);
+                    importJson.SetValue("/bufferViews/212/byteStride", 4);
+                    importJson.SetValue("/bufferViews/213/byteStride", 4);
+                    importJson.SetValue("/bufferViews/214/byteStride", 4);
+                    importJson.SetValue("/bufferViews/215/byteStride", 4);
+                    importJson.SetValue("/bufferViews/243/byteStride", 4);
+                    importJson.SetValue("/bufferViews/247/byteStride", 64);
+                    importJson.SetValue("/bufferViews/248/byteStride", 64);
+                    importJson.SetValue("/bufferViews/249/byteStride", 64);
+                    importJson.SetValue("/bufferViews/250/byteStride", 64);
+                    importJson.SetValue("/bufferViews/251/byteStride", 64);
+                    importJson.SetValue("/bufferViews/252/byteStride", 64);
+                    importJson.SetValue("/bufferViews/253/byteStride", 64);
+                    */
+                    importedJson.RemoveValue(Utf8String.From("/bufferViews/*/byteStride"));
+
+                    var vrm = VRMExporter.Export(UniGLTF.MeshExportSettings.Default, context.Root);
+
+                    // TODO: Check contents in JSON
+                    /*var exportJson = */
+                    JsonParser.Parse(vrm.ToJson());
+
+                    // TODO: Check contents in JSON
+                    /*var newExportedJson = */
+                    // JsonParser.Parse(JsonSchema.FromType<glTF>().Serialize(vrm));
+
+                    /*
+                    foreach (var kv in importJson.Diff(exportJson))
+                    {
+                        Debug.Log(kv);
+                    }
+
+                    Assert.AreEqual(importJson, exportJson);
+                    */
+                }
+                finally
+                {
+                    UnityEngine.Object.DestroyImmediate(destroyer.gameObject);
+                }
             }
         }
 
