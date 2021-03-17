67b083b10fa165f5ff27dadab4cb549ad6227117
ousttrue <ousttrue@gmail.com>
2021-03-17 19:59:45 +0900

    add LoadTests
    https://github.com/KhronosGroup/glTF-Sample-Models

---
+++ Assets/UniGLTF/Tests/UniGLTF/LoadTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace UniGLTF
+{
+    public class LoadTests
+    {
+        static IEnumerable<FileInfo> EnumerateGltfFiles(DirectoryInfo dir)
+        {
+            if (dir.Name == ".git")
+            {
+                yield break;
+            }
+
+            foreach (var child in dir.EnumerateDirectories())
+            {
+                foreach (var x in EnumerateGltfFiles(child))
+                {
+                    yield return x;
+                }
+            }
+
+            foreach (var child in dir.EnumerateFiles())
+            {
+                switch (child.Extension.ToLower())
+                {
+                    case ".gltf":
+                    case ".glb":
+                        yield return child;
+                        break;
+                }
+            }
+        }
+
+        static void Load(FileInfo gltf)
+        {
+            var parser = new GltfParser();
+            try
+            {
+                parser.ParsePath(gltf.FullName);
+            }
+            catch (Exception)
+            {
+                Debug.LogError($"ParseError: {gltf}");
+                throw;
+            }
+
+            try
+            {
+                using (var importer = new ImporterContext(parser))
+                {
+                    importer.Load();
+                }
+            }
+            catch (Exception)
+            {
+                Debug.LogError($"LoadError: {gltf}");
+                throw;
+            }
+        }
+
+        [Test]
+        public void GltfSampleModelsTests()
+        {
+            var env = System.Environment.GetEnvironmentVariable("GLTF_SAMPLE_MODELS");
+            if (string.IsNullOrEmpty(env))
+            {
+                return;
+            }
+            var root = new DirectoryInfo(env);
+            if (!root.Exists)
+            {
+                return;
+            }
+
+            foreach (var gltf in EnumerateGltfFiles(root))
+            {
+                Load(gltf);
+            }
+        }
+    }
+}
