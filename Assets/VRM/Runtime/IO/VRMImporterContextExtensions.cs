2528f8aa6cc85d1c6c2dddd479354ca807e5388e
ousttrue <ousttrue@gmail.com>
2021-03-19 16:58:49 +0900

    ReadMeta 復活

---
+++ Assets/VRM/Runtime/IO/VRMImporterContextExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace VRM
+{
+    public static class VRMImporterContextExtensions
+    {
+        public static VRMMetaObject ReadMeta(this VRMImporterContext context, bool createThumbnail = false)
+        {
+            var task = context.ReadMetaAsync(default(UniGLTF.ImmediateCaller), createThumbnail);
+            task.Wait();
+            return task.Result;
+        }
+    }
+}
