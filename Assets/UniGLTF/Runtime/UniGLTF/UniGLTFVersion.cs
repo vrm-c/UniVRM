160d92e69dd0e52337113464b0cc4d92844983df
hiroj <j-hirose@virtualcast.jp>
2021-04-22 18:17:43 +0900

    update v0.67.3

--- Assets/UniGLTF/Runtime/UniGLTF/UniGLTFVersion.cs
+++ Assets/UniGLTF/Runtime/UniGLTF/UniGLTFVersion.cs
@@ -5,7 +5,7 @@ namespace UniGLTF
     {
         public const int MAJOR = 2;
         public const int MINOR = 3;
-        public const int PATCH = 2;
-        public const string VERSION = "2.3.2";
+        public const int PATCH = 3;
+        public const string VERSION = "2.3.3";
     }
 }
