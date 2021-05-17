cc3568da14459150a229d80ff42b44a26a558df6
ousttrue <ousttrue@gmail.com>
2021-05-17 17:44:38 +0900

    Inverse

--- Assets/VRM10/Runtime/Components/Constraint/TR.cs
+++ Assets/VRM10/Runtime/Components/Constraint/TR.cs
@@ -36,5 +36,11 @@ namespace UniVRM10
         public Matrix4x4 TRS(float s) => Matrix4x4.TRS(Translation, Rotation, new Vector3(s, s, s));
 
         public static TR operator *(TR a, TR b) => new TR(a.Rotation * b.Rotation, a.Rotation * b.Translation + a.Translation);
+
+        public TR Inverse()
+        {
+            var inv = Quaternion.Inverse(Rotation);
+            return new TR(inv, inv * Translation);
+        }
     }
 }
