00814c02746a45ef69067c708028924854f944e5
ousttrue <ousttrue@gmail.com>
2021-05-18 17:36:05 +0900

    コード整理。m_destinationCoordinate, ModelRoot を追加

--- Assets/VRM10/Runtime/Components/Constraint/VRM10AimConstraint.cs
+++ Assets/VRM10/Runtime/Components/Constraint/VRM10AimConstraint.cs
@@ -1,53 +1,34 @@
 ﻿using System;
+using UniGLTF.Extensions.VRMC_node_constraint;
 using UnityEngine;
 
 
 namespace UniVRM10
 {
-    /// <summary>
-    /// WIP
-    /// 
-    /// Slerp(thisRot * offsetRot(Aim x Up), sourceRot, weight)
-    /// 
-    /// </summary>
     [DisallowMultipleComponent]
     public class VRM10AimConstraint : VRM10Constraint
     {
         [SerializeField]
-        public Transform Source = default;
-
-        [SerializeField]
         [Range(0, 10.0f)]
         public float Weight = 1.0f;
 
-        public Quaternion ParentRotation => transform.parent == null ? Quaternion.identity : transform.parent.rotation;
+        [SerializeField]
+        public Transform ModelRoot = default;
+
+        [Header("Source")]
+        [SerializeField]
+        public Transform Source = default;
+
+        [Header("Destination")]
+        [SerializeField]
+        ObjectSpace m_destinationCoordinate = default;
 
         [SerializeField]
         public Quaternion DestinationOffset = Quaternion.identity;
 
-        public class AimLogic
-        {
-            public readonly Quaternion InitialLocalRotation;
-            public AimLogic(Quaternion initialLocalRotation)
-            {
-                InitialLocalRotation = initialLocalRotation;
-            }
-        }
-        public AimLogic Logic { get; private set; }
+        public ConstraintSource m_src;
 
-        void Start()
-        {
-            if (Source == null)
-            {
-                enabled = false;
-                return;
-            }
-
-            if (Logic == null)
-            {
-                Logic = new AimLogic(transform.localRotation);
-            }
-        }
+        public Quaternion Delta;
 
         /// <summary>
         /// TargetのUpdateよりも先か後かはその時による。
@@ -55,21 +36,24 @@ namespace UniVRM10
         /// </summary>
         public override void Process()
         {
-            if (Logic == null)
+            if (Source == null)
             {
+                enabled = false;
                 return;
+            }
+
+            if (m_src == null)
+            {
+                m_src = new ConstraintSource(Source, ModelRoot);
             }
 
             var zAxis = (Source.position - transform.position).normalized;
             var xAxis = Vector3.Cross(Vector3.up, zAxis);
             var yAxis = Vector3.Cross(zAxis, xAxis);
             var m = new Matrix4x4(xAxis, yAxis, zAxis, new Vector4(0, 0, 0, 1));
-            Delta = Quaternion.Inverse(ParentRotation * Logic.InitialLocalRotation * DestinationOffset) * m.rotation;
-            transform.rotation = ParentRotation * Logic.InitialLocalRotation * Delta;
+            var parent = TR.FromParent(transform);
+            Delta = Quaternion.Inverse(parent.Rotation * m_src.LocalInitial.Rotation * DestinationOffset) * m.rotation;
+            transform.rotation = parent.Rotation * m_src.LocalInitial.Rotation * Delta;
         }
-
-        public float Yaw;
-        public float Pitch;
-        public Quaternion Delta;
     }
 }
