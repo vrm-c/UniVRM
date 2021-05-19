9dd09ebf0057d11ddc1fe65ba81da913028cb539
ousttrue <ousttrue@gmail.com>
2021-05-19 17:43:18 +0900

    rename

--- Assets/VRM/Editor/VrmPreference.cs
+++ Assets/VRM/Editor/VrmPreference.cs
@@ -4,7 +4,7 @@ using System.Linq;
 
 namespace VRM
 {
-    public static class VrmInfo
+    public static class VrmPreference
     {
         const string KEY_STOP_VRMASSETPOSTPROCESSOR = "StopVrmAssetPostProcessor";
         const string ASSETPOSTPROCESSOR_STOP_SYMBOL = "VRM_STOP_ASSETPOSTPROCESSOR";
