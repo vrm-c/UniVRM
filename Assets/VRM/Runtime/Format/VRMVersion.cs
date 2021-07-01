aa94e5fe90d3c7d89b0ce443390562c5f82e9578
Masataka SUMI <santarh@gmail.com>
2021-07-01 16:06:07 +0900

    update version v0.67.9

--- Assets/VRM/Runtime/Format/VRMVersion.cs
+++ Assets/VRM/Runtime/Format/VRMVersion.cs
@@ -5,7 +5,7 @@ namespace VRM
     {
         public const int MAJOR = 0;
         public const int MINOR = 67;
-        public const int PATCH = 8;
-        public const string VERSION = "0.67.8";
+        public const int PATCH = 9;
+        public const string VERSION = "0.67.9";
     }
 }
