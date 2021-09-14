abc6610fe2ce371e6992eab22ceaa5e80fec0e93
ousttrue <ousttrue@gmail.com>
2021-09-14 13:48:14 +0900

    locale 作成

--- docs/conf.py
+++ docs/conf.py
@@ -53,3 +53,6 @@ html_theme = 'classic'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+locale_dirs = ['locale/']
+gettext_compact = False
