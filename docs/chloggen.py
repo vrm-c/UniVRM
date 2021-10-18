3552fccc2d0a3ac988e3b378cd8d2cc8048589e8
ousttrue <ousttrue@gmail.com>
2021-10-18 15:09:10 +0900

    UniVRM-0.87.0 release note

--- docs/chloggen.py
+++ docs/chloggen.py
@@ -3,14 +3,18 @@ import sys
 import git.repo
 import re
 import pathlib
+import release_gen
 
 HERE = pathlib.Path(__file__).absolute().parent
 
 MERGE_PATTERN = re.compile(r'Merge pull request #(\d+)')
 
 
-def main(repo: git.repo.Repo, rev: str):
-    print('# v0.86.0: 1.0準備')
+def main(repo: git.repo.Repo, version: str):
+    major, minor, patch = [int(x) for x in version.split('.')]
+    rev = f'v{major}.{minor-1}.0..v{major}.{minor}.0'
+
+    print(f'# v{version}: 1.0準備')
     print()
     for item in repo.iter_commits(rev=rev):
         m = MERGE_PATTERN.match(item.message)
@@ -26,4 +30,5 @@ def main(repo: git.repo.Repo, rev: str):
 
 if __name__ == '__main__':
     repo = git.repo.Repo(str(HERE.parent))
-    main(repo, sys.argv[1])
+    version = release_gen.get_version()
+    main(repo, version)
