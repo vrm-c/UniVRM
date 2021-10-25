1e98f065e1d58f223739008bdc7d1419630f2fd7
notargs <satoyutaka94@gmail.com>
2021-10-25 17:03:47 +0900

    Fix errors

--- Assets/VRM10/Runtime/IO/Material/Vrm10MaterialDescriptorGenerator.cs
+++ Assets/VRM10/Runtime/IO/Material/Vrm10MaterialDescriptorGenerator.cs
@@ -1,4 +1,6 @@
-﻿using UniGLTF;
+﻿using System;
+using System.Collections.Generic;
+using UniGLTF;
 using UnityEngine;
 using VRMShaders;
 
@@ -9,23 +11,23 @@ namespace UniVRM10
         public MaterialDescriptor Get(GltfData data, int i)
         {
             // mtoon
-            if (!Vrm10MToonMaterialImporter.TryCreateParam(data, i, out MaterialDescriptor matDesc))
-            {
-                // unlit
-                if (!GltfUnlitMaterialImporter.TryCreateParam(data, i, out matDesc))
-                {
-                    // pbr
-                    if (!GltfPbrMaterialImporter.TryCreateParam(data, i, out matDesc))
-                    {
-                        // fallback
+            if (Vrm10MToonMaterialImporter.TryCreateParam(data, i, out MaterialDescriptor matDesc)) return matDesc;
+            // unlit
+            if (GltfUnlitMaterialImporter.TryCreateParam(data, i, out matDesc)) return matDesc;
+            // pbr
+            if (GltfPbrMaterialImporter.TryCreateParam(data, i, out matDesc)) return matDesc;
+            // fallback
 #if VRM_DEVELOP
-                        Debug.LogWarning($"material: {i} out of range. fallback");
+            Debug.LogWarning($"material: {i} out of range. fallback");
 #endif
-                        return new MaterialDescriptor(GltfMaterialDescriptorGenerator.GetMaterialName(i, null), GltfPbrMaterialImporter.ShaderName);
-                    }
-                }
-            }
-            return matDesc;
+            return new MaterialDescriptor(
+                GltfMaterialDescriptorGenerator.GetMaterialName(i, null), GltfPbrMaterialImporter.ShaderName, 
+                null, 
+                new Dictionary<string, TextureDescriptor>(),
+                new Dictionary<string, float>(),
+                new Dictionary<string, Color>(),
+                new Dictionary<string, Vector4>(),
+                new Action<Material>[]{});
         }
 
     }
