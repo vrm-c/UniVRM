1c2180793b6bffefac6017838b1416ad318a4d78
ousttrue <ousttrue@gmail.com>
2021-10-26 13:09:30 +0900

    remove unused IStorage.GetPath

--- Assets/UniGLTF/Runtime/UniGLTF/Format/IStorage.cs
+++ Assets/UniGLTF/Runtime/UniGLTF/Format/IStorage.cs
@@ -5,12 +5,5 @@ namespace UniGLTF
     public interface IStorage
     {
         ArraySegment<Byte> Get(string url = default);
-
-        /// <summary>
-        /// Get original filepath if exists
-        /// </summary>
-        /// <param name="url"></param>
-        /// <returns></returns>
-        string GetPath(string url);
     }
 }
