448339ad255b57d577d0e9ed818fc93a03b4e8d2
ousttrue <ousttrue@gmail.com>
2021-11-30 19:39:26 +0900

    UniVRM-0.90.1

--- Assets/UniGLTF/Runtime/UniGLTF/UniGLTFVersion.cs
+++ Assets/UniGLTF/Runtime/UniGLTF/UniGLTFVersion.cs
@@ -5,7 +5,7 @@ namespace UniGLTF
     {
         public const int MAJOR = 2;
         public const int MINOR = 26;
-        public const int PATCH = 0;
-        public const string VERSION = "2.26.0";
+        public const int PATCH = 1;
+        public const string VERSION = "2.26.1";
     }
 }
--- Assets/VRM/Runtime/Format/VRMVersion.cs
+++ Assets/VRM/Runtime/Format/VRMVersion.cs
@@ -5,7 +5,7 @@ namespace VRM
     {
         public const int MAJOR = 0;
         public const int MINOR = 90;
-        public const int PATCH = 0;
-        public const string VERSION = "0.90.0";
+        public const int PATCH = 1;
+        public const string VERSION = "0.90.1";
     }
 }
