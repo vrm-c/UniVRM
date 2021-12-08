78bbdf516239a067a63beed34c400c09730e9240
Masataka SUMI <santarh@gmail.com>
2021-12-08 20:59:55 +0900

    見やすく

--- Assets/UniGLTF/Editor/TopMenu.cs
+++ Assets/UniGLTF/Editor/TopMenu.cs
@@ -12,52 +12,31 @@ namespace UniGLTF
         private const string UserGltfMenuPrefix = UniGLTFVersion.MENU;
         private const string UserMeshUtilityPrefix = UniGLTFVersion.MENU + "/Mesh Utility";
         private const string DevelopmentMenuPrefix = UniGLTFVersion.MENU + "/Development";
-        private const int UserGltfMenuPriority = 0;
-        private const int UserMeshUtilityMenuPriority = 10;
-        private const int DevelopmentMenuPriority = 30;
 
-        [MenuItem(
-            UserGltfMenuPrefix + "/Version: " + UniGLTFVersion.UNIGLTF_VERSION,
-            validate = true)]
+        [MenuItem(UserGltfMenuPrefix + "/Version: " + UniGLTFVersion.UNIGLTF_VERSION, validate = true)]
         private static bool ShowVersionValidation() => false;
 
-        [MenuItem(
-            UserGltfMenuPrefix + "/Version: " + UniGLTFVersion.UNIGLTF_VERSION,
-            priority = UserGltfMenuPriority + 0)]
-        private static void ShowVersion()
-        {
-        }
+        [MenuItem(UserGltfMenuPrefix + "/Version: " + UniGLTFVersion.UNIGLTF_VERSION, priority = 0)]
+        private static void ShowVersion() { }
 
-        [MenuItem(
-            UserGltfMenuPrefix + "/Export to GLB",
-            priority = UserGltfMenuPriority + 1)]
+        [MenuItem(UserGltfMenuPrefix + "/Export to GLB", priority = 1)]
         private static void ExportGameObjectToGltf() => TopMenuImplementation.ExportGameObjectToGltfFile();
 
-        [MenuItem(
-            UserGltfMenuPrefix + "/Import from GLTF (*.gltf|*.glb|*.zip)",
-            priority = UserGltfMenuPriority + 2)]
+        [MenuItem(UserGltfMenuPrefix + "/Import from GLTF (*.gltf|*.glb|*.zip)", priority = 2)]
         private static void ImportGltfFile() => TopMenuImplementation.ImportGltfFileToGameObject();
 
-        [MenuItem(
-            UserMeshUtilityPrefix + "/MeshProcessing Wizard",
-            priority = UserMeshUtilityMenuPriority + 0)]
+        [MenuItem(UserMeshUtilityPrefix + "/MeshProcessing Wizard", priority = 10)]
         private static void OpenMeshProcessingWindow() => TopMenuImplementation.OpenMeshProcessingWindow();
 
-        [MenuItem(
-            UserMeshUtilityPrefix + "/Open Documents",
-            priority = UserMeshUtilityMenuPriority + 1)]
+        [MenuItem(UserMeshUtilityPrefix + "/Open Documents", priority = 11)]
         private static void MeshUtilityDocs() => Application.OpenURL("https://vrm.dev/en/docs/univrm/gltf/mesh_utility/");
 
 #if VRM_DEVELOP
-        [MenuItem(
-            DevelopmentMenuPrefix + "/Generate Serialization Code",
-            priority = DevelopmentMenuPriority + 1)]
+        [MenuItem(DevelopmentMenuPrefix + "/Generate Serialization Code", priority = 20)]
         private static void GenerateSerializationCode() => TopMenuImplementation.GenerateSerializationCode();
 
 
-        [MenuItem(
-            DevelopmentMenuPrefix + "/Generate UniJSON ConcreteCast",
-            priority = DevelopmentMenuPriority + 2)]
+        [MenuItem(DevelopmentMenuPrefix + "/Generate UniJSON ConcreteCast", priority = 21)]
         private static void GenerateUniJsonConcreteCastCode() => UniJSON.ConcreteCast.GenerateGenericCast();
 #endif
     }
