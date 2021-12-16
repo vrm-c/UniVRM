500b59d2aee5b36211bec9145ade3f2550367a32
ousttrue <ousttrue@gmail.com>
2021-12-16 18:45:16 +0900

    Test 追加。Empty_extensionUsed

--- Assets/UniGLTF/Tests/UniGLTF/GltfJsonUtilTests.cs
+++ Assets/UniGLTF/Tests/UniGLTF/GltfJsonUtilTests.cs
@@ -88,5 +88,42 @@ namespace UniGLTF
             Assert.AreEqual(new string[] { "KHR_materials_unlit" },
             parsed["extensionUsed"].ArrayItems().Select(x => x.GetString()).ToArray());
         }
+
+        [Test]
+        public void Empty_extensionUsed()
+        {
+            var dst = GltfJsonUtil.FindUsedExtensionsAndUpdateJson(@"{
+    ""asset"": {
+        ""generator"": ""COLLADA2GLTF"",
+        ""version"": ""2.0""
+    },
+    ""scene"": 0,
+    ""scenes"": [
+        {
+            ""nodes"": [
+                0
+            ]
+    }
+    ],
+    ""extensionUsed"": [""dummy""],
+    ""materials"": [
+        {
+            ""pbrMetallicRoughness"": {
+                ""baseColorFactor"": [
+                    0.800000011920929,
+                    0.0,
+                    0.0,
+                    1.0
+                ],
+                ""metallicFactor"": 0.0
+            },
+            ""name"": ""Red""
+        }
+    ]
+}");
+
+            var parsed = dst.ParseAsJson();
+            Assert.False(parsed.ContainsKey("extensionUsed"));
+        }
     }
 }
