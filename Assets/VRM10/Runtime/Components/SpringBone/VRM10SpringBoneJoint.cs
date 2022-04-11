d9280d0aa506ba1d465098b55cf5e5780f7e3501
ousttrue <ousttrue@gmail.com>
2022-04-11 17:09:43 +0900

    [ContextMenu("AddJointsToAllChild0")]

--- Assets/VRM10/Runtime/Components/SpringBone/VRM10SpringBoneJoint.cs
+++ Assets/VRM10/Runtime/Components/SpringBone/VRM10SpringBoneJoint.cs
@@ -8,6 +8,7 @@ using UnityEditor;
 namespace UniVRM10
 {
     [Serializable]
+    [DisallowMultipleComponent]
     public class VRM10SpringBoneJoint : MonoBehaviour
     {
         [SerializeField, Range(0, 4), Header("Settings")]
@@ -27,5 +28,79 @@ namespace UniVRM10
 
         [SerializeField, Range(0, 0.5f), Header("Collision")]
         public float m_jointRadius = 0.02f;
+
+        void AddJointRecursive(Transform t)
+        {
+            var joint = t.gameObject.GetComponent<VRM10SpringBoneJoint>();
+            if (joint == null)
+            {
+                joint = t.gameObject.AddComponent<VRM10SpringBoneJoint>();
+                Debug.Log($"{joint} added");
+            }
+
+            if (t.childCount > 0)
+            {
+                // only first child
+                AddJointRecursive(t.GetChild(0));
+            }
+        }
+
+        void GetJoints(Transform t, List<VRM10SpringBoneJoint> joints)
+        {
+            var joint = t.GetComponent<VRM10SpringBoneJoint>();
+            if (joint != null)
+            {
+                joints.Add(joint);
+            }
+
+            if (t.childCount > 0)
+            {
+                GetJoints(t.GetChild(0), joints);
+            }
+        }
+
+        [ContextMenu("AddJointsToAllChild0")]
+        private void AddJointsToChild0()
+        {
+            var root = GetComponentInParent<Vrm10Instance>();
+            if (root == null)
+            {
+                Debug.LogWarning("not Vrm10Instance");
+                return;
+            }
+
+            if (transform.childCount == 0)
+            {
+                Debug.LogWarning("no children");
+                return;
+            }
+
+            AddJointRecursive(transform.GetChild(0));
+
+            // updater root
+            foreach (var spring in root.SpringBone.Springs)
+            {
+                for (int i = 0; i < spring.Joints.Count; ++i)
+                {
+                    if (spring.Joints[i] == this)
+                    {
+                        // found
+                        while (spring.Joints.Count - 1 > i)
+                        {
+                            // remove after this joint
+                            spring.Joints.RemoveAt(spring.Joints.Count - 1);
+                        }
+                        // get descendants joints
+                        var joints = new List<VRM10SpringBoneJoint>();
+                        GetJoints(transform.GetChild(0), joints);
+                        // add jonits to after this
+                        spring.Joints.AddRange(joints);
+                        return;
+                    }
+                }
+            }
+
+            Debug.LogWarning($"{this} is found in {root}");
+        }
     }
 }
