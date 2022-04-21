c9186f913cf5667277bea3c2a1ba41d900366381
ousttrue <ousttrue@gmail.com>
2022-04-21 17:23:20 +0900

    UniVRM-0.98.1

--- Assets/VRM/Runtime/Format/VRMVersion.cs
+++ Assets/VRM/Runtime/Format/VRMVersion.cs
@@ -5,7 +5,7 @@ namespace VRM
     {
         public const int MAJOR = 0;
         public const int MINOR = 98;
-        public const int PATCH = 0;
-        public const string VERSION = "0.98.0";
+        public const int PATCH = 1;
+        public const string VERSION = "0.98.1";
     }
 }
