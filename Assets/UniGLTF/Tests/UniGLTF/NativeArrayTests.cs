c29da363df5b2b6101034c593facee06da0f446b
ousttrue <ousttrue@gmail.com>
2022-06-10 13:39:26 +0900

    挙動の変化に追随

--- Assets/UniGLTF/Tests/UniGLTF/NativeArrayTests.cs
+++ Assets/UniGLTF/Tests/UniGLTF/NativeArrayTests.cs
@@ -6,6 +6,26 @@ namespace UniGLTF
 {
     public class NativeArrayTests
     {
+#if UNITY_2020_3_OR_NEWER
+        [Test]
+        public void DisposeTest()
+        {
+            var array = new NativeArray<byte>(64, Allocator.Persistent);
+            var sub = array.GetSubArray(10, 4);
+
+            // SubArray の Dispose が可能になった ! (Unity-2020.3)
+            // Assert.Throws<InvalidOperationException>(() => { sub.Dispose(); });
+            var cast = array.Reinterpret<int>(1);
+
+            // Dispose可能
+            cast.Dispose();
+
+            // ObjectDisposedException に変わった ! (Unity-2020.3)
+            Assert.Throws<ObjectDisposedException>(() => { var c = cast[0]; });
+            Assert.Throws<ObjectDisposedException>(() => { var a = array[0]; });
+            Assert.Throws<ObjectDisposedException>(() => { var s = sub[0]; });
+        }
+#else
         [Test]
         public void DisposeTest()
         {
@@ -22,5 +42,6 @@ namespace UniGLTF
             Assert.Throws<InvalidOperationException>(() => { var a = array[0]; });
             Assert.Throws<InvalidOperationException>(() => { var s = sub[0]; });
         }
+#endif
     }
 }
