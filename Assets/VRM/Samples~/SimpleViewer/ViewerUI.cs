7d6f835ffe0cccc2467bc80f2824a86d2f4190aa
ousttrue <ousttrue@gmail.com>
2022-09-13 14:39:40 +0900

    UniVRM-0.103.1

--- Assets/VRM/Samples~/SimpleViewer/ViewerUI.cs
+++ Assets/VRM/Samples~/SimpleViewer/ViewerUI.cs
@@ -327,7 +327,7 @@ namespace VRM.SimpleViewer
             string[] cmds = System.Environment.GetCommandLineArgs();
             if (cmds.Length > 1)
             {
-                LoadModelAsync(cmds[1]);
+                LoadPathAsync(cmds[1]);
             }
 
             m_texts.Start();
@@ -391,10 +391,20 @@ namespace VRM.SimpleViewer
                 return;
             }
 
-            LoadModelAsync(path);
+            LoadPathAsync(path);
         }
 
-        async void LoadModelAsync(string path, byte[] bytes = null)
+        async void LoadPathAsync(string path)
+        {
+            if (!File.Exists(path))
+            {
+                Debug.LogWarning($"{path} not exists");
+                return;
+            }
+            LoadModelAsync(path, File.ReadAllBytes(path));
+        }
+
+        async void LoadModelAsync(string path, byte[] bytes)
         {
             var size = bytes != null ? bytes.Length : 0;
             Debug.Log($"LoadModelAsync: {path}: {size}bytes");
@@ -406,9 +416,10 @@ namespace VRM.SimpleViewer
                 case ".glb":
                 case ".zip":
                     {
-                        var instance = await GltfUtility.LoadBytesAsync(path, bytes,
+                        var instance = await GltfUtility.LoadAsync(path,
                             GetIAwaitCaller(m_useAsync.isOn),
                             GetGltfMaterialGenerator(m_useUrpMaterial.isOn));
+                        SetModel(instance);
                         break;
                     }
 
