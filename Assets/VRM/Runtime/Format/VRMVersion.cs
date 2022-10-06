4ca7df4d1cbdf76c5b9dab2460da735fec74fa52
ousttrue <ousttrue@gmail.com>
2022-10-06 21:20:31 +0900

    UniVRM-0.105.0.
    サンプルの更新があるので差分多めです。XXX_Samples から XXX/Samples~ へのコピーが version 更新のついでに動作します。

--- Assets/VRM/Runtime/Format/VRMVersion.cs
+++ Assets/VRM/Runtime/Format/VRMVersion.cs
@@ -4,8 +4,8 @@ namespace VRM
     public static partial class VRMVersion
     {
         public const int MAJOR = 0;
-        public const int MINOR = 104;
-        public const int PATCH = 2;
-        public const string VERSION = "0.104.2";
+        public const int MINOR = 105;
+        public const int PATCH = 0;
+        public const string VERSION = "0.105.0";
     }
 }
