8fae79a5be760ad01990ba9aeae58c9e668fa064
Masataka SUMI <santarh@gmail.com>
2023-01-12 13:18:17 +0900

    UniVRM-0.107.2

--- Assets/UniGLTF/Runtime/UniGLTF/UniGLTFVersion.cs
+++ Assets/UniGLTF/Runtime/UniGLTF/UniGLTFVersion.cs
@@ -5,7 +5,7 @@ namespace UniGLTF
     {
         public const int MAJOR = 2;
         public const int MINOR = 43;
-        public const int PATCH = 1;
-        public const string VERSION = "2.43.1";
+        public const int PATCH = 2;
+        public const string VERSION = "2.43.2";
     }
 }
