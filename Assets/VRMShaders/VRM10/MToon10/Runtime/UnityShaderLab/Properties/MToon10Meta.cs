9f6927716351757cbd1fa71945b158d257419e52
notargs <satoyutaka94@gmail.com>
2023-05-08 17:12:37 +0900

    C#側をShader名変更に追従

--- Assets/VRMShaders/VRM10/MToon10/Runtime/UnityShaderLab/Properties/MToon10Meta.cs
+++ Assets/VRMShaders/VRM10/MToon10/Runtime/UnityShaderLab/Properties/MToon10Meta.cs
@@ -3,6 +3,6 @@
     public static class MToon10Meta
     {
         public static readonly string UnityShaderName = "VRM10/MToon10";
-        public static readonly string URPUnityShaderName = "VRM10/MToon10_URP";
+        public static readonly string URPUnityShaderName = "VRM10/Universal Render Pipeline/MToon10";
     }
 }
