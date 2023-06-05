32bc5d6d866530f255fc20247137b82578e8c29e
ousttrue <ousttrue@gmail.com>
2023-06-05 17:07:50 +0900

    UniVRM-0.111.0
    バージョン番号の更新。サンプルの更新による差分あり。

--- Assets/UniGLTF/Runtime/UniGLTF/UniGLTFVersion.cs
+++ Assets/UniGLTF/Runtime/UniGLTF/UniGLTFVersion.cs
@@ -4,8 +4,8 @@ namespace UniGLTF
     public static partial class UniGLTFVersion
     {
         public const int MAJOR = 2;
-        public const int MINOR = 46;
+        public const int MINOR = 47;
         public const int PATCH = 0;
-        public const string VERSION = "2.46.0";
+        public const string VERSION = "2.47.0";
     }
 }
