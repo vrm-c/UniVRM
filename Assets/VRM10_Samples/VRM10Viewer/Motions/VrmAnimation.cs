464a5c895326a89840ed2ba4acd604e5bd8bafbc
ousttrue <ousttrue@gmail.com>
2023-06-14 16:45:42 +0900

    VrmAnimationInstance

--- Assets/VRM10_Samples/VRM10Viewer/Motions/VrmAnimation.cs
+++ Assets/VRM10_Samples/VRM10Viewer/Motions/VrmAnimation.cs
@@ -9,50 +9,33 @@ namespace UniVRM10.VRM10Viewer
 {
     public class VrmAnimation : IMotion
     {
-        private RuntimeGltfInstance m_instance;
-        public SkinnedMeshRenderer m_boxMan;
-        public SkinnedMeshRenderer BoxMan => m_boxMan;
-        private (INormalizedPoseProvider, ITPoseProvider) m_controlRig;
-        (INormalizedPoseProvider, ITPoseProvider) IMotion.ControlRig => m_controlRig;
+        private readonly VrmAnimationInstance m_instance;
+
+        (INormalizedPoseProvider, ITPoseProvider) IMotion.ControlRig => m_instance.ControlRig;
         public IDictionary<ExpressionKey, Transform> ExpressionMap { get; } = new Dictionary<ExpressionKey, Transform>();
 
-        public VrmAnimation(RuntimeGltfInstance instance)
+        public VrmAnimation(VrmAnimationInstance instance)
         {
             m_instance = instance;
             if (instance.GetComponent<Animation>() is Animation animation)
             {
                 animation.Play();
             }
-
-            var humanoid = instance.gameObject.AddComponent<Humanoid>();
-            if (humanoid.AssignBonesFromAnimator())
-            {
-                var provider = new InitRotationPoseProvider(instance.transform, humanoid);
-                m_controlRig = (provider, provider);
-
-                // create SkinnedMesh for bone visualize
-                var animator = instance.GetComponent<Animator>();
-                m_boxMan = SkeletonMeshUtility.CreateRenderer(animator);
-                var material = new Material(Shader.Find("Standard"));
-                BoxMan.sharedMaterial = material;
-                var mesh = BoxMan.sharedMesh;
-                mesh.name = "box-man";
-            }
         }
 
         public void ShowBoxMan(bool enable)
         {
-            if (m_boxMan != null)
+            if (m_instance.BoxMan != null)
             {
-                m_boxMan.enabled = enable;
+                m_instance.BoxMan.enabled = enable;
             }
         }
 
         public void Dispose()
         {
-            if (m_boxMan != null)
+            if (m_instance.BoxMan != null)
             {
-                GameObject.Destroy(m_boxMan.gameObject);
+                GameObject.Destroy(m_instance.BoxMan.gameObject);
             }
         }
 
@@ -64,7 +47,10 @@ namespace UniVRM10.VRM10Viewer
                 loader.InvertAxis = Axes.X;
                 var instance = await loader.LoadAsync(new ImmediateCaller());
 
-                var animation = new VrmAnimation(instance);
+                var animationInstance = instance.gameObject.AddComponent<UniVRM10.VrmAnimationInstance>();
+                animationInstance.Initialize();
+
+                var animation = new VrmAnimation(animationInstance);
                 foreach (var (preset, transform) in loader.GetExpressions())
                 {
                     animation.ExpressionMap.Add(preset, transform);
