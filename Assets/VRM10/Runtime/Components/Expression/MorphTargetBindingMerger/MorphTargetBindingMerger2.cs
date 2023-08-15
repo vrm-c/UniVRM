95f405dae9a4db79404c0ad786920cbaa8356230
Masataka SUMI <santarh@gmail.com>
2023-08-15 21:20:57 +0900

    Add Alternative Implementation

---
+++ Assets/VRM10/Runtime/Components/Expression/MorphTargetBindingMerger/MorphTargetBindingMerger2.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace UniVRM10
+{
+    internal sealed class MorphTargetBindingMerger2
+    {
+        private readonly Dictionary<ExpressionKey, Dictionary<RuntimeMorphTargetBinding, float>> _weights = new(ExpressionKey.Comparer);
+        private readonly Dictionary<RuntimeMorphTargetBinding, (bool, float)> _accumulatedWeights = new(RuntimeMorphTargetBinding.Comparer);
+        private readonly RuntimeMorphTargetBinding[] _targetIds;
+
+        public MorphTargetBindingMerger2(Dictionary<ExpressionKey, VRM10Expression> expressions, Transform modelRoot)
+        {
+            foreach (var (expressionKey, expression) in expressions)
+            {
+                _weights.Add(expressionKey, new Dictionary<RuntimeMorphTargetBinding, float>(RuntimeMorphTargetBinding.Comparer));
+                foreach (var binding in expression.MorphTargetBindings)
+                {
+                    var id = RuntimeMorphTargetBinding.Create(binding, modelRoot);
+                    if (id == null)
+                    {
+                        Debug.LogWarning($"Invalid MorphTargetBinding found: {binding}");
+                        continue;
+                    }
+                    if (_weights[expressionKey].ContainsKey(id.Value))
+                    {
+                        Debug.LogWarning($"Duplicate MorphTargetBinding found: {binding}");
+                        continue;
+                    }
+                    _weights[expressionKey].Add(id.Value, binding.Weight * MorphTargetBinding.VRM_TO_UNITY);
+                    _accumulatedWeights.TryAdd(id.Value, (false, 0f));
+                }
+            }
+            _targetIds = _accumulatedWeights.Keys.ToArray();
+        }
+
+        public void AccumulateValue(ExpressionKey key, float value)
+        {
+            if (_weights.TryGetValue(key, out var weightsPerBinding))
+            {
+                foreach (var (id, weight) in weightsPerBinding)
+                {
+                    var (_, currentWeight) = _accumulatedWeights[id];
+                    _accumulatedWeights[id] = (true, currentWeight + weight * value);
+                }
+            }
+        }
+
+        public void Apply()
+        {
+            foreach (var (id, (isAccumulated, totalWeight)) in _accumulatedWeights)
+            {
+                if (isAccumulated)
+                {
+                    if (id.TargetRenderer == null) continue;
+
+                    id.TargetRenderer.SetBlendShapeWeight(id.TargetBlendShapeIndex, totalWeight);
+                }
+            }
+
+            // NOTE: Reset
+            foreach (var id in _targetIds)
+            {
+                _accumulatedWeights[id] = (false, 0f);
+            }
+        }
+    }
+}
---
+++ Assets/VRM10/Runtime/Components/Expression/MorphTargetBindingMerger/RuntimeMorphTargetBinding.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UniVRM10
+{
+    internal readonly struct RuntimeMorphTargetBinding : IEquatable<RuntimeMorphTargetBinding>
+    {
+        public static RuntimeMorphTargetBinding? Create(MorphTargetBinding binding, Transform modelRoot)
+        {
+            if (modelRoot == null) return null;
+
+            var targetGameObject = modelRoot.Find(binding.RelativePath);
+            if (targetGameObject == null) return null;
+
+            var targetRenderer = targetGameObject.GetComponent<SkinnedMeshRenderer>();
+            if (targetRenderer == null) return null;
+            if (targetRenderer.sharedMesh == null) return null;
+            if (targetRenderer.sharedMesh.blendShapeCount <= binding.Index) return null;
+
+            return new RuntimeMorphTargetBinding(targetRenderer, binding.Index, binding.Weight * MorphTargetBinding.VRM_TO_UNITY);
+        }
+
+        public SkinnedMeshRenderer TargetRenderer { get; }
+        public int TargetRendererInstanceId { get; } // NOTE: Compare key
+        public int TargetBlendShapeIndex { get; } // NOTE: Compare key
+        public float Weight { get; }
+
+        private RuntimeMorphTargetBinding(SkinnedMeshRenderer targetRenderer, int targetBlendShapeIndex, float weight)
+        {
+            TargetRenderer = targetRenderer;
+            TargetRendererInstanceId = TargetRenderer.GetInstanceID();
+            TargetBlendShapeIndex = targetBlendShapeIndex;
+            Weight = weight;
+        }
+
+        public bool Equals(RuntimeMorphTargetBinding other)
+        {
+            return TargetRendererInstanceId == other.TargetRendererInstanceId && TargetBlendShapeIndex == other.TargetBlendShapeIndex;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is RuntimeMorphTargetBinding other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(TargetRendererInstanceId, TargetBlendShapeIndex);
+        }
+
+        public static IEqualityComparer<RuntimeMorphTargetBinding> Comparer { get; } = new EqualityComparer();
+
+        private sealed class EqualityComparer : IEqualityComparer<RuntimeMorphTargetBinding>
+        {
+            public bool Equals(RuntimeMorphTargetBinding x, RuntimeMorphTargetBinding y)
+            {
+                return x.TargetRendererInstanceId == y.TargetRendererInstanceId && x.TargetBlendShapeIndex == y.TargetBlendShapeIndex;
+            }
+
+            public int GetHashCode(RuntimeMorphTargetBinding obj)
+            {
+                return HashCode.Combine(obj.TargetRendererInstanceId, obj.TargetBlendShapeIndex);
+            }
+        }
+    }
+}
