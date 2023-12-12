10d8ba2f9aaf8c1bd2d68068b661930b8b11e56c
ousttrue <ousttrue@gmail.com>
2023-12-12 13:07:36 +0900

    UniVRM-0.116.0

--- Assets/UniGLTF/Runtime/UniGLTF/UniGLTFVersion.cs
+++ Assets/UniGLTF/Runtime/UniGLTF/UniGLTFVersion.cs
@@ -4,8 +4,8 @@ namespace UniGLTF
     public static partial class UniGLTFVersion
     {
         public const int MAJOR = 2;
-        public const int MINOR = 51;
+        public const int MINOR = 52;
         public const int PATCH = 0;
-        public const string VERSION = "2.51.0";
+        public const string VERSION = "2.52.0";
     }
 }
