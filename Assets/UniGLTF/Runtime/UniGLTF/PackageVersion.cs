a71e60ce30539242e4e2011e01ea8cfe3e0f0df1
ousttrue <ousttrue@gmail.com>
2024-01-09 14:33:17 +0900

    UniVRM-0.117

--- Assets/UniGLTF/Runtime/UniGLTF/PackageVersion.cs
+++ Assets/UniGLTF/Runtime/UniGLTF/PackageVersion.cs
@@ -4,8 +4,8 @@ namespace UniGLTF
     public static partial class PackageVersion
     {
         public const int MAJOR = 0;
-        public const int MINOR = 116;
+        public const int MINOR = 117;
         public const int PATCH = 0;
-        public const string VERSION = "0.116.0";
+        public const string VERSION = "0.117.0";
     }
 }
