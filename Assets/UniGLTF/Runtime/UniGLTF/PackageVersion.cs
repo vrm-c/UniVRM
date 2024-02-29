38d4f3e69bfda67c766e0d58d908fa1d73919eeb
ousttrue <ousttrue@gmail.com>
2024-02-29 20:25:09 +0900

    sample 更新ひとつあり。

--- Assets/UniGLTF/Runtime/UniGLTF/PackageVersion.cs
+++ Assets/UniGLTF/Runtime/UniGLTF/PackageVersion.cs
@@ -4,8 +4,8 @@ namespace UniGLTF
     public static partial class PackageVersion
     {
         public const int MAJOR = 0;
-        public const int MINOR = 119;
+        public const int MINOR = 120;
         public const int PATCH = 0;
-        public const string VERSION = "0.119.0";
+        public const string VERSION = "0.120.0";
     }
 }
