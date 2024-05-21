551ad8e3f2e8d52d28b9f0a6e5208724f9171563
Masataka SUMI <santarh@gmail.com>
2024-05-21 12:29:29 +0900

    Add the test whether the load function can finish synchronously.

---
+++ Assets/VRM10/Tests/Vrm10ApiTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+using UniVRM10;
+using VRMShaders;
+
+namespace VRM10.Tests
+{
+    public sealed class Vrm10ApiTests
+    {
+        [Test]
+        public void LoadImmediately()
+        {
+            var loadTask = Vrm10.LoadPathAsync(
+                TestAsset.AliciaPath,
+                canLoadVrm0X: true,
+                awaitCaller: new ImmediateCaller()
+            );
+
+            Assert.AreEqual(true, loadTask.IsCompleted);
+        }
+    }
+}
