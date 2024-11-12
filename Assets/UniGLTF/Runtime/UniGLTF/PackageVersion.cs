0bfef39e3c55eb3a2edbf1f8656f4caf4d28705d
ousttrue <ousttrue@gmail.com>
2024-11-12 16:45:15 +0900

    UniVRM-0.128.0
    Unity 2022.3.52f1

--- Assets/UniGLTF/Runtime/UniGLTF/PackageVersion.cs
+++ Assets/UniGLTF/Runtime/UniGLTF/PackageVersion.cs
@@ -4,8 +4,8 @@ namespace UniGLTF
     public static partial class PackageVersion
     {
         public const int MAJOR = 0;
-        public const int MINOR = 127;
-        public const int PATCH = 2;
-        public const string VERSION = "0.127.2";
+        public const int MINOR = 128;
+        public const int PATCH = 0;
+        public const string VERSION = "0.128.0";
     }
 }
