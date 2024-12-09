267b080eace116892e2ac0bcadda749d1731384c
ousttrue <ousttrue@gmail.com>
2024-12-09 16:27:33 +0900

    Editor の filedialog の platform 依存を隔離

---
+++ Assets/UniGLTF/Editor/UniGltfEditorDialog.cs
@@ -0,0 +1,91 @@
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace UniGLTF
+{
+    public static class UniGltfEditorDialog
+    {
+#if UNITY_EDITOR_WIN
+        // #if false
+        public const string IMPORT_MENU_NAME = "Import glTF... (*.gltf|*.glb|*.zip)";
+        static bool IsWindows()
+        {
+            return true;
+        }
+#else
+        public const string IMPORT_MENU_NAME = "Import glTF... (*.glb)";
+        static bool IsWindows()
+        {
+            return false;
+        }
+#endif
+
+        static string extensionString(string[] extensions)
+        {
+            if (IsWindows())
+            {
+                return string.Join(",", extensions);
+            }
+            else
+            {
+                // in OSX multi extension cause exception.
+                // https://github.com/vrm-c/UniVRM/issues/1837
+                // in Linux
+                // https://github.com/vrm-c/UniVRM/issues/2515
+                return extensions.Length > 0 ? extensions[0] : "";
+            }
+        }
+
+        public static bool TryOpenFilePanel(string directory, out string path)
+        {
+            path = EditorUtility.OpenFilePanel(IMPORT_MENU_NAME, directory,
+                // https://github.com/vrm-c/UniVRM/issues/1837
+                IsWindows() ? "gltf,glb,zip" : "glb"
+            );
+            if (string.IsNullOrEmpty(path))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        static string s_lastExportDir;
+        public static bool TrySaveFilePanel(string title, string name, string[] extensions, out string path)
+        {
+            string directory = s_lastExportDir;
+            if (string.IsNullOrEmpty(directory))
+            {
+                directory = Directory.GetParent(Application.dataPath).ToString();
+            }
+
+            path = EditorUtility.SaveFilePanel(title, directory, name, extensionString(extensions));
+            if (string.IsNullOrEmpty(path))
+            {
+                return false;
+            }
+
+            s_lastExportDir = Path.GetDirectoryName(path).Replace("\\", "/");
+            return true;
+        }
+
+        public static bool TryGetDir(string title, string dir, out string path)
+        {
+            string directory = string.IsNullOrEmpty(dir) ? s_lastExportDir : dir;
+            if (string.IsNullOrEmpty(directory))
+            {
+                directory = Directory.GetParent(Application.dataPath).ToString();
+            }
+
+            path = EditorUtility.SaveFolderPanel(title, directory, null);
+            if (string.IsNullOrEmpty(path))
+            {
+                return false;
+            }
+
+            s_lastExportDir = Path.GetDirectoryName(path).Replace("\\", "/");
+            return true;
+        }
+    }
+}
