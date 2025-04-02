2651ba3fc487ba0a544c9b6f65425fab90fd23d8
amamagi <amausagiii@gmail.com>
2025-04-02 12:09:01 +0900

    add showcase template

---
+++ Assets/VRM10_Samples/MToon10Showcase/Runtime/ShowcaseTemplates/UVAnimationShowcase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using VRM10.MToon10;
+
+namespace VRM10.Samples.MToon10Showcase
+{
+#if VRM_DEVELOP
+    [CreateAssetMenu(menuName = "VRM10/Samples/MToon10Showcase/" + nameof(UVAnimationShowcase))]
+#endif
+    public sealed class UVAnimationShowcase : ScriptableObject
+    {
+        public Material baseMaterial;
+        public UVAnimationEntry[] entries;
+
+        [Serializable]
+        public sealed class UVAnimationEntry
+        {
+            public Texture uvAnimationMaskTexture;
+            public float uvAnimationScrollXSpeedFactor;
+            public float uvAnimationScrollYSpeedFactor;
+            public float uvAnimationRotationSpeedFactor;
+        }
+    }
+}
