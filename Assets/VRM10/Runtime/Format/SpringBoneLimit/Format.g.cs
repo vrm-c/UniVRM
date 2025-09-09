a9e9ec7b3b26944973b348f9ee0d8bd202f5202e
ousttrue <ousttrue@gmail.com>
2025-09-09 15:01:21 +0900

    generate

---
+++ Assets/VRM10/Runtime/Format/SpringBoneLimit/Format.g.cs
@@ -0,0 +1,91 @@
+﻿// This file is generated from JsonSchema. Don't modify this source code.
+using System;
+using System.Collections.Generic;
+
+
+namespace UniGLTF.Extensions.VRMC_springBone_limit
+{
+
+    public class ConeLimit
+    {
+        // Dictionary object with extension-specific objects.
+        public object Extensions;
+
+        // Application-specific data.
+        public object Extras;
+
+        // The angle of the cone limit in radians. If the angle is set to π or greater, the angle will be interpreted as π by the implementation. When the angle is set to π, the cone shape becomes a sphere.
+        public float? Angle;
+
+        // The rotation from the default orientation of the cone limit. The rotation is represented as a quaternion (x, y, z, w), where w is the scalar.
+        public float[] Rotation;
+    }
+
+    public class HingeLimit
+    {
+        // Dictionary object with extension-specific objects.
+        public object Extensions;
+
+        // Application-specific data.
+        public object Extras;
+
+        // The angle of the hinge limit in radians. If the angle is set to π or greater, the angle will be interpreted as π by the implementation. When the angle is set to π, the hinge shape becomes a full disc.
+        public float? Angle;
+
+        // The rotation from the default orientation of the hinge limit. The rotation is represented as a quaternion (x, y, z, w), where w is the scalar.
+        public float[] Rotation;
+    }
+
+    public class SphericalLimit
+    {
+        // Dictionary object with extension-specific objects.
+        public object Extensions;
+
+        // Application-specific data.
+        public object Extras;
+
+        // The phi angle of the spherical limit in radians. If the phi angle is set to π or greater, the angle will be interpreted as π by the implementation.
+        public float? Phi;
+
+        // The theta angle of the spherical limit in radians. If the theta angle is set to π/2 or greater, the angle will be interpreted as π/2 by the implementation.
+        public float? Theta;
+
+        // The rotation from the default orientation of the spherical limit. The rotation is represented as a quaternion (x, y, z, w), where w is the scalar.
+        public float[] Rotation;
+    }
+
+    public class Limit
+    {
+        // Dictionary object with extension-specific objects.
+        public object Extensions;
+
+        // Application-specific data.
+        public object Extras;
+
+        // A limit that restricts the orientation of the spring in a cone shape.
+        public ConeLimit Cone;
+
+        // A limit that restricts the orientation of the spring in a hinge shape.
+        public HingeLimit Hinge;
+
+        // A limit that restricts the orientation of the spring in a spherical coordinate shape.
+        public SphericalLimit Spherical;
+    }
+
+    public class VRMC_springBone_limit
+    {
+        public const string ExtensionName = "VRMC_springBone_limit";
+
+        // Dictionary object with extension-specific objects.
+        public object Extensions;
+
+        // Application-specific data.
+        public object Extras;
+
+        // Specification version of VRMC_springBone_limit.
+        public string SpecVersion;
+
+        // Describes a limit apply to the spring. Either cone, hinge, or spherical must be present.
+        public Limit Limit;
+    }
+}
---
+++ Assets/VRM10/Runtime/Format/SpringBoneLimit/Serializer.g.cs
@@ -0,0 +1,218 @@
+﻿// This file is generated from JsonSchema. Don't modify this source code.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UniJSON;
+
+namespace UniGLTF.Extensions.VRMC_springBone_limit {
+
+    static public class GltfSerializer
+    {
+
+        public static void SerializeTo(ref UniGLTF.glTFExtension dst, VRMC_springBone_limit extension)
+        {
+            if (dst is glTFExtensionImport)
+            {
+                throw new NotImplementedException();
+            }
+
+            if (!(dst is glTFExtensionExport extensions))
+            {
+                extensions = new glTFExtensionExport();
+                dst = extensions;
+            }
+
+            var f = new JsonFormatter();
+            Serialize(f, extension);
+            extensions.Add(VRMC_springBone_limit.ExtensionName, f.GetStoreBytes());
+        }
+
+
+public static void Serialize(JsonFormatter f, VRMC_springBone_limit value)
+{
+    f.BeginMap();
+
+
+    if(value.Extensions!=null){
+        f.Key("extensions");                
+        (value.Extensions as glTFExtension).Serialize(f);
+    }
+
+    if(value.Extras!=null){
+        f.Key("extras");                
+        (value.Extras as glTFExtension).Serialize(f);
+    }
+
+    if(!string.IsNullOrEmpty(value.SpecVersion)){
+        f.Key("specVersion");                
+        f.Value(value.SpecVersion);
+    }
+
+    if(value.Limit!=null){
+        f.Key("limit");                
+        Serialize_Limit(f, value.Limit);
+    }
+
+    f.EndMap();
+}
+
+public static void Serialize_Limit(JsonFormatter f, Limit value)
+{
+    f.BeginMap();
+
+
+    if(value.Extensions!=null){
+        f.Key("extensions");                
+        (value.Extensions as glTFExtension).Serialize(f);
+    }
+
+    if(value.Extras!=null){
+        f.Key("extras");                
+        (value.Extras as glTFExtension).Serialize(f);
+    }
+
+    if(value.Cone!=null){
+        f.Key("cone");                
+        __limit_Serialize_Cone(f, value.Cone);
+    }
+
+    if(value.Hinge!=null){
+        f.Key("hinge");                
+        __limit_Serialize_Hinge(f, value.Hinge);
+    }
+
+    if(value.Spherical!=null){
+        f.Key("spherical");                
+        __limit_Serialize_Spherical(f, value.Spherical);
+    }
+
+    f.EndMap();
+}
+
+public static void __limit_Serialize_Cone(JsonFormatter f, ConeLimit value)
+{
+    f.BeginMap();
+
+
+    if(value.Extensions!=null){
+        f.Key("extensions");                
+        (value.Extensions as glTFExtension).Serialize(f);
+    }
+
+    if(value.Extras!=null){
+        f.Key("extras");                
+        (value.Extras as glTFExtension).Serialize(f);
+    }
+
+    if(value.Angle.HasValue){
+        f.Key("angle");                
+        f.Value(value.Angle.GetValueOrDefault());
+    }
+
+    if(value.Rotation!=null&&value.Rotation.Count()>=4){
+        f.Key("rotation");                
+        __limit__cone_Serialize_Rotation(f, value.Rotation);
+    }
+
+    f.EndMap();
+}
+
+public static void __limit__cone_Serialize_Rotation(JsonFormatter f, float[] value)
+{
+    f.BeginList();
+
+    foreach(var item in value)
+    {
+    f.Value(item);
+
+    }
+    f.EndList();
+}
+
+public static void __limit_Serialize_Hinge(JsonFormatter f, HingeLimit value)
+{
+    f.BeginMap();
+
+
+    if(value.Extensions!=null){
+        f.Key("extensions");                
+        (value.Extensions as glTFExtension).Serialize(f);
+    }
+
+    if(value.Extras!=null){
+        f.Key("extras");                
+        (value.Extras as glTFExtension).Serialize(f);
+    }
+
+    if(value.Angle.HasValue){
+        f.Key("angle");                
+        f.Value(value.Angle.GetValueOrDefault());
+    }
+
+    if(value.Rotation!=null&&value.Rotation.Count()>=4){
+        f.Key("rotation");                
+        __limit__hinge_Serialize_Rotation(f, value.Rotation);
+    }
+
+    f.EndMap();
+}
+
+public static void __limit__hinge_Serialize_Rotation(JsonFormatter f, float[] value)
+{
+    f.BeginList();
+
+    foreach(var item in value)
+    {
+    f.Value(item);
+
+    }
+    f.EndList();
+}
+
+public static void __limit_Serialize_Spherical(JsonFormatter f, SphericalLimit value)
+{
+    f.BeginMap();
+
+
+    if(value.Extensions!=null){
+        f.Key("extensions");                
+        (value.Extensions as glTFExtension).Serialize(f);
+    }
+
+    if(value.Extras!=null){
+        f.Key("extras");                
+        (value.Extras as glTFExtension).Serialize(f);
+    }
+
+    if(value.Phi.HasValue){
+        f.Key("phi");                
+        f.Value(value.Phi.GetValueOrDefault());
+    }
+
+    if(value.Theta.HasValue){
+        f.Key("theta");                
+        f.Value(value.Theta.GetValueOrDefault());
+    }
+
+    if(value.Rotation!=null&&value.Rotation.Count()>=4){
+        f.Key("rotation");                
+        __limit__spherical_Serialize_Rotation(f, value.Rotation);
+    }
+
+    f.EndMap();
+}
+
+public static void __limit__spherical_Serialize_Rotation(JsonFormatter f, float[] value)
+{
+    f.BeginList();
+
+    foreach(var item in value)
+    {
+    f.Value(item);
+
+    }
+    f.EndList();
+}
+
+    } // class
+} // namespace
